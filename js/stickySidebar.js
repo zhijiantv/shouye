f5ff81a5d533bc891c0df36facd4c809e6e92532
zhijiantv <15757546431@163.com>
2019-11-26 18:18:36 +0800

    js

--- js/stickySidebar.js
+++ js/stickySidebar.js
@@ -55,5 +55,54 @@
         });
 
     };
+//判断用户使用的设备
+var deviceVal  = browserRedirect();
+function browserRedirect() {
+  var sUserAgent = navigator.userAgent.toLowerCase();
+  var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+  var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+  var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+  var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+  var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+  var bIsAndroid = sUserAgent.match(/android/i) == "android";
+  var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+  var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+  if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+    return 'phone';
+  } else {
+    return 'pc';
+  }
+}
 
+/*
+ * 2018-6-12更新
+ * 解决在低分辨率下首页内容过高导致滚动条出现，影响主题背景图片不全屏的问题
+ * 解决方式：对于屏幕分辨率高度低于845px显示器，改变首页的内容为6块区域。
+ */
+
+ (function(){
+  //当浏览器窗口被调整大小时触发
+  window.onresize = function(){
+    ShowHideElement("i-link-box","linkList-item",845);
+  }
+  window.onload = function(){
+    ShowHideElement("i-link-box","linkList-item",845); 
+  }
+  function ShowHideElement(Element1, Element2, Vaule){
+    var Person = document.getElementsByClassName(Element1);
+    var BoxHeight = document.getElementsByClassName(Element2);
+    var WindowHeight = window.innerHeight||document.body.clientHeight;
+    //遍历获取到的元素
+    for(var i=6; i<Person.length; i++){
+      if(WindowHeight <= Vaule && deviceVal === "pc"){
+        Person[i].style.display = "none";
+        BoxHeight[0].style.marginTop="5px";
+      }else{
+        Person[i].style.display = "block";
+        BoxHeight[0].style.marginTop="0px";
+      }
+    }
+  }
+  window.ShowHideElement = ShowHideElement;
+}());
 }(jQuery));
